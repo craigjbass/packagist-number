93dd4f34c82040fed906290522020f65d1f2252e
Craig Bass <craig@devls.co.uk>
2016-02-09 20:58:57 +0000

    Initial commit with failing test

---
+++ tests/TestTest.php
@@ -0,0 +1,14 @@
+<?php
+
+class TestTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @test
+     */
+    public function GivenTest()
+    {
+        $this->assertTrue( false );
+    }
+
+}
